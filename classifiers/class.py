ae6eb66877d315707fbc6c24ace37b9bf96a5a41
mayns <oksgorobets@gmail.com>
2014-11-08 17:35:31 +0300

    2 lection

---
+++ classifiers/class.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+import math
+
+__author__ = 'oks'
+
+zoo = []
+with open(u'zoo.data.txt', u'r') as f:
+    for l in f:
+        h = l.strip().split(u',')
+        assert len(h) == 18
+        zoo.append(h)
+
+print zoo
+
+
+def entropy(items):
+    labels = {}
+    for item in items:
+        label = item[17]
+        labels.setdefault(label, 0)
+        labels[label] += 1
+    e = 0.0
+    for n in labels.values():
+        # probability
+        p = float(n) / len(items)
+        e += - p * math.log(p)
+    return e
+
+print entropy(zoo)
+mammals = [i for i in zoo if i[17] == u'1']
+print mammals
+print entropy(mammals)
+
+# ENTROPY MAX VALUE
+
+
+def information_gain(items, i):
+    groups = {}
+    gain = entropy(items)
+    for item in items:
+        value = item[i]
+        groups.setdefault(value, []).append(item)
+    for group in groups.values():
+        p = float(len(group)) / len(items)
+        e = entropy(group)
+        gain -= p*e
+    return gain
+
+for i in xrange(1, 17):
+    print i, information_gain(zoo, i)
+
+# DECISION TREE - построить дерево и проверить насколько хорошо получилось (данные поделить на две части)
